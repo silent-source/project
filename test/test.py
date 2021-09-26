d499f65f400921ec42262c6e49a96b8b631d50cb
silent-source <56764149+silent-source@users.noreply.github.com>
2021-09-26 14:37:17 +0800

    Delete test.py

--- test/test.py
+++
@@ -1,9 +0,0 @@
-print("success read!")
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument( '--name',help='enter the test name')
-parser.add_argument('--param',default='test param')
-args = parser.parse_args()
-print("args:",args)
-for i in range(20):
-    print(i)
